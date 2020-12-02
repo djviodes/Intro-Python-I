8caf995f44ca0358ac7c4a9a158c654e334c2988
djviodes <djviodes26@gmail.com>
2020-12-02 09:54:12 -0800

    Finished comprehensions.py finally

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,6 +11,10 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
+for x in range(6):
+    y.append(x)
+
+y.pop(0)
 
 print (y)
 
@@ -18,6 +22,8 @@ print (y)
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
+for x in range(10):
+    y.append(x**3)
 
 print(y)
 
@@ -27,6 +33,8 @@ print(y)
 a = ["foo", "bar", "baz"]
 
 y = []
+for x in range(3):
+    y.append(a[x].upper())
 
 print(y)
 
@@ -36,6 +44,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [i for i in x if int(i)%2 == 0]
 
 print(y)
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -34,14 +34,24 @@ waypoints = [
 ]
 
 # Add a new waypoint to the list
-# YOUR CODE HERE
+waypoints.append(
+    {
+        "lat": 44,
+        "lon": -124,
+        "name": "a fourth place"
+    }
+)
+
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
-# YOUR CODE HERE
+waypoints[0] = [n.replace('-121', '-130') for n in waypoints[0]]
+
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
